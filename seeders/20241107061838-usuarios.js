00cd3ab92266e6a57a476361ec260b23c4d7308e
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2024-12-05 16:41:20 -0600

    Update seed data with new password hashes for usuarios

--- seeders/20241107061838-usuarios.js
+++ seeders/20241107061838-usuarios.js
@@ -15,8 +15,8 @@ module.exports = {
     ]);
 
     await queryInterface.bulkInsert('usuario', [
-      { id: crypto.randomUUID(), email: 'gvera@uv.mx', passwordhash: await bcrypt.hash('patito', 10), nombre: 'Guillermo Vera', rolid: AdministradorUUID, protegido: true, createdAt: new Date(), updatedAt: new Date() },
-      { id: crypto.randomUUID(), email: 'patito@uv.mx', passwordhash: await bcrypt.hash('patito', 10), nombre: 'Usuario patito', rolid: UsuarioUUID, createdAt: new Date(), updatedAt: new Date() }
+      { id: crypto.randomUUID(), email: 'gvera@uv.mx', passwordhash: await bcrypt.hash('@PatitoONM3', 10), nombre: 'Guillermo Vera', rolid: AdministradorUUID, protegido: true, createdAt: new Date(), updatedAt: new Date() },
+      { id: crypto.randomUUID(), email: 'patito@uv.mx', passwordhash: await bcrypt.hash('@PatitoONM3', 10), nombre: 'Usuario patito', rolid: UsuarioUUID, createdAt: new Date(), updatedAt: new Date() }
     ]);
   },
 
