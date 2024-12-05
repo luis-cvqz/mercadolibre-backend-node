0d24065b1edfcd45c8119267f539520ee96bc190
eduar <eduarcaco@hotmail.com>
2024-12-05 16:52:51 -0600

    Create Client Validations

--- controllers/clientes.controller.js
+++ controllers/clientes.controller.js
@@ -1,16 +1,36 @@
 const { usuario, rol} = require('../models')
+const { body, validationResult } = require('express-validator')
 const bcrypt = require('bcrypt')
 const crypto = require('crypto')
 
 let self = {}
 
+self.usuarioValidator = [
+    body('email', 'El campo email es obligatorio').not().isEmpty().isLength({ max: 255 }),
+    body('nombre', 'El campo nombre es obligatorio').not().isEmpty().isLength({ max: 255 }),
+    body('password', 'El campo password es obligatorio').not().isEmpty().isLength({ max: 255 }),
+]
+
 // POST: api/clientes
 self.create = async function (req, res, next) {
     try {
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()){
+            res.status(400).json(errors)
+            return
+        } 
 
        if (req.body.password !== req.body.confirmPassword) {
             return res.status(400).json({
                 error: "La contraseña y la confirmación de la contraseña no coinciden."
+            });
+        }
+
+        const existingUser = await usuario.findOne({ where: { email: req.body.email } });
+        if(existingUser) {
+            return res.status(400).json({
+                error: "El correo electrónico ya está en uso. Ingrese uno diferente."
             });
         }
 
