ec375bbd6282d23f47f7c86b5718e8add0566282
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2024-12-05 16:57:42 -0600

    Add validation rules for cliente creation

--- controllers/clientes.controller.js
+++ controllers/clientes.controller.js
@@ -1,12 +1,26 @@
 const { usuario, rol} = require('../models')
 const bcrypt = require('bcrypt')
 const crypto = require('crypto')
+const { body, validationResult } = require('express-validator');
 
 let self = {}
+
+self.clienteValidator = [
+    body('email', 'El campo email es obligatorio').not().isEmpty().isLength({ max: 255 }),
+    body('nombre', 'El campo nombre es obligatorio').not().isEmpty().isLength({ max: 255 }),
+    body('password', 'El campo password es obligatorio').not().isEmpty().isLength({ max: 255 })
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,12}$/, 
+            'i').withMessage('La contraseña debe tener al menos 8 caracteres, máximo 12 caracteres, una mayúscula, una minúscula, un número y un carácter especial.')
+]
 
 // POST: api/clientes
 self.create = async function (req, res, next) {
     try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()){
+            res.status(400).json(errors)
+            return
+        }
 
        if (req.body.password !== req.body.confirmPassword) {
             return res.status(400).json({
